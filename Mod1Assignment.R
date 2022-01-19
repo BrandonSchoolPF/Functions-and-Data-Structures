9ea16c202ba2dd025328d610c802ee82a3bdb365
Brandon Ly <brandonly@Brandons-MBP.home>
2022-01-19 17:14:10 -0500

    First Commit for assignment

---
+++ Mod1Assignment.R
@@ -0,0 +1,17 @@
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+
+#This function does not work because "assignment" and "someData" does not exist
+myMean <- function(assignment2) {
+  return(sum(assignment)/length(someData))
+}
+
+myMean(assignment2)
+
+
+#Corrected function
+myMean_corrected <- function(assignment2) {
+  return(sum(assignment2)/length(assignment2))
+}
+
+myMean_corrected(assignment2)
